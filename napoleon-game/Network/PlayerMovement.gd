0d0fbce8729911c53ad609b488b4dbc6f0cc81e2
air <callanjsmith420@gmail.com>
2026-05-05 22:05:55 +0800

    Reverted to pre-multiplayer
    I deleted all the multiplayer stuff im gonna try again tomorrow

--- napoleon-game/Network/PlayerMovement.gd
+++ napoleon-game/Network/PlayerMovement.gd
@@ -3,17 +3,16 @@ extends CharacterBody3D
 @export var speed := 5.0
 @export var jump_velocity := 4.5
 @export var mouse_sensitivity := 0.002
-@onready var camera = $Camera3D
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
-var rotation_x := 0.0
+@onready var camera = $Camera3D
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
-	
+
 	if Input.is_action_pressed("move_forward"):
 		direction -= transform.basis.z
 	if Input.is_action_pressed("move_backwards"):
@@ -22,27 +21,28 @@ func _physics_process(delta):
 		direction -= transform.basis.x
 	if Input.is_action_pressed("move_right"):
 		direction += transform.basis.x
-	
+
 	direction = direction.normalized()
-	
+
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
-	
+
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-	
+
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
-		
+
 	move_and_slide()
-	
+
+var rotation_x := 0.0
+
 func _input(event):
 	if event is InputEventMouseMotion:
+		# Rotate player left/right
 		rotate_y(-event.relative.x * mouse_sensitivity)
-		
+
 		rotation_x -= event.relative.y * mouse_sensitivity
 		rotation_x = clamp(rotation_x, deg_to_rad(-90), deg_to_rad(90))
-		
-		camera.rotation.x = rotation.x
-	
-	
+
+		camera.rotation.x = rotation_x
